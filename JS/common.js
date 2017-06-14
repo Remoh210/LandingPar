23c21eb784ba73ac4fb453d6b898f29b2f8536f6
Bondarenko Bogdan <bondarenkobogdankiev@gmail.com>
2017-06-14 16:12:04 +0900

    2

--- JS/common.js
+++ JS/common.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	$(window).scroll(function(event) {
-		var st = $(this).scrollTop();
+		var st = $('html').scrollTop() || $('body').scrollTop();
 		$('.header_text').css({
 			"transform" : "translate(0%, " + st/5 + "%"
 	});
 
-		$('.sect_2 img').css({
+		$('.sect_2').css({
 			"transform" : "translate(0%, " + st/30 + "%"
 
 	});
