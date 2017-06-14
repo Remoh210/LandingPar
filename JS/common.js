6d423f31f0c3a72988d35447a5bf2b521df72bba
PC <l2myfgame@mail.ru>
2017-06-14 15:27:44 +0900

    new add

--- JS/common.js
+++ JS/common.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-	$('p').click(function(event) {
-		$(this).hide();
+	$(window).scroll(function(event) {
+		var st = $(this).scrollTop();
+		$('.header_text').css({
+			"transform" : "translate(0%, " + st/5 + "%"
 	});
+
+		$('.sect_2 img').css({
+			"transform" : "translate(0%, " + st/30 + "%"
+
+	});
+});
+
 });
