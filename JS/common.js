4d701056e2839e8404fc164d075d6ad36a9ba4f9
Bondarenko Bogdan <bondarenkobogdankiev@gmail.com>
2017-06-15 10:28:08 +0900

    fix safari

--- JS/common.js
+++ JS/common.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
 	$(window).scroll(function(event) {
 		var st = $('html').scrollTop() || $('body').scrollTop();
-		$('.header_text').css({
-			"transform" : "translate(0%, " + st/5 + "%"
+		$('.header_text').css({"-webkit-transform":"translate(0%," + st/5 + "%)"
+		// $('.header_text').css({
+		// 	"transform" : "translate(0%, " + st/5 + "%"
 	});
 
 		$('.sect_2').css({
-			"transform" : "translate(0%, " + st/30 + "%"
+			"-webkit-transform" : "translate(0%, " + st/30 + "%)"
 
 	});
 
 				$('.sect_3_txt').css({
-			"transform" : "translate(0%, " + st/7 + "%"
+			"-webkit-transform" : "translate(0%, " + st/7 + "%)"
 
 	});
 
 
 				$('.sect_2_txt').css({
-			"transform" : "translate(0%, -" + st/2 + "%"
+			"-webkit-transform" : "translate(0%, -" + st/2 + "%)"
 
 	});
 });
